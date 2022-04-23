13793175f94d79c990c39795991190b53e62c599
Ramon Domingos <ramongemio@gmail.com>
2022-04-23 01:53:23 -0300

    .

--- api/src/controllers/PurchaseController.ts
+++ api/src/controllers/PurchaseController.ts
@@ -3,7 +3,9 @@ import { Request, Response } from 'express';
 import nixService from '../services/nix';
 const PurchaseController = {
   async store(req: Request, res: Response) {},
-  async show(req: Request, res: Response) {},
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+  },
   async showByUser(req: Request, res: Response) {},
 };
 export default PurchaseController;
